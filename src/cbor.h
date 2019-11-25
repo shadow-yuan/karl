e27d74867ac3f8bebfe236f22b6ee2c209cb7bc3
shadow-yuan <1092421495@qq.com>
2019-11-25 12:35:44 +0800

    fix: operands don't affect result

--- src/cbor.h
+++ src/cbor.h
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// A c++ simple json library
+// An easy to use c++ json library
 // Version 1.0.0
 // https://github.com/shadow-yuan/karl
 //
